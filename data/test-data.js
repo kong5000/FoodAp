0e42dbd325ea86b49d12714db1142f43e6bb9cf7
kong5000 <keith.ong5000@gmail.com>
2020-06-07 10:16:07 -0700

    Add navigation and dummy cuisine data

---
+++ data/test-data.js
@@ -0,0 +1,12 @@
+import Cuisine from '../models/cuisine'
+
+export const CUISINES = [
+    new Cuisine('id1', 'Pizza', 'red'),
+    new Cuisine('id1', 'Burgers', 'blue'),
+    new Cuisine('id2', 'Chinese', 'green'),
+    new Cuisine('id3', 'French', 'blue'),
+    new Cuisine('id4', 'Italian', 'red'),
+    new Cuisine('id5', 'Thai', 'purple'),
+    new Cuisine('id6', 'Greek', 'blue'),
+    new Cuisine('id6', 'Indian', 'green'),
+]
--- screens/CuisineSelectionScreen.js
+++ screens/CuisineSelectionScreen.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { CUISINES } from '../data/test-data'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import Theme from '../constants/Theme'
+
+const renderCuisineCard = (itemData) => {
+    return <View style={styles.cuisineCard}><Text>{itemData.item.title}</Text></View>
+}
 
 const CuisineSelectionScreen = props => {
     return (
-        <View style={styles.screen}>
-            <Text>Cuisine Select Screen!</Text>
-            <Button title="Go to Menu" onPress={() => props.navigation.navigate('Menu')}/>
-        </View>
+        <FlatList numColumns={2} data={CUISINES} renderItem={renderCuisineCard} />
     );
 };
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
+    screen: {
+        flex: 1,
         justifyContent: "center",
         alignItems: "center"
+    },
+    cuisineCard: {
+        flex: 1,
+        margin: 20,
+        height: 200
     }
+
 })
+
+CuisineSelectionScreen.navigationOptions = {
+    headerTitle: 'Cuisines',
+    headerStyle:{
+        backgroundColor: Theme.primaryColor
+    },
+    headerTintColor:'white'
+}
 
 export default CuisineSelectionScreen;
